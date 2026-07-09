fdbe3e32a146853dc3d9017eaebf0d60fe8ab3aa
Joshua Aziken <joshaziken@gmail.com>
2026-07-09 18:49:14 +0100

    Complete Lesson 1 - Variables

---
+++ 01_python_basics/exercises.py
@@ -0,0 +1,25 @@
+book_title = "Diary of a wimpy kid"
+author = "Jeff Kinney"
+rating = 7.8
+pages = 224
+genre = "Children's fiction"
+publication_year = 2007
+
+print ("Book Title:", book_title)
+print ("Author:", author)
+print ("Rating:", rating)
+print ("Pages:", pages)
+print ("Genre:", genre)
+print ("Publication Year:", publication_year)
+
+student_name = "Joshua Aziken"
+University = "University of Hull"
+Course = "Masters in Artificial Intelligence and Data Science"
+Country = "United Kingdom"
+Career_goal = "Data Scientist"
+
+print ("Student Name:", student_name)
+print ("University:", University)
+print ("Course:", Course)
+print ("Country:", Country)
+print ("Career Goal:", Career_goal)
