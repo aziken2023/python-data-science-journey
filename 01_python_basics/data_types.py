5f97d5427a92882269e4b96fa1e117447717b6a3
Joshua Aziken <joshaziken@gmail.com>
2026-07-11 22:20:08 +0100

    Learning to convert datatypes

---
+++ 01_python_basics/data_types.py
@@ -0,0 +1,34 @@
+#string data type
+book_title = "Diary of a wimpy kid"
+
+#int data type
+pages = "224"
+
+#Float data type
+rating = 7.8
+
+#Boolean data type
+is_available = True
+
+#none data type
+publisher = None 
+
+#Print the data types of the variables
+print(type(book_title))
+print(type(pages))
+print(type(rating))
+print(type(is_available))
+print(type(publisher))
+
+#Learning to convert variables from one data type to another
+age = "22"
+salary = "50000.50"
+student = "True"
+
+#Convert the string variables to their respective data types
+age = int(age)
+salary =float(salary)
+
+#print new variable types
+print(type(age))
+print(type(salary))
