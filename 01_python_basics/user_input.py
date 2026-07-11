aed9efaba28e9d3b7fcf6cf8dfc4075b95aea2e5
Joshua Aziken <joshaziken@gmail.com>
2026-07-11 23:10:41 +0100

    Created a new pyhton file for data inputs and working with inputting data

---
+++ 01_python_basics/user_input.py
@@ -0,0 +1,13 @@
+#Create an input to ask for book title
+book_title = input("Enter the book title:")
+
+#Create an input for the rest of the book
+author = input("What is the authors name?")
+pages = int(input("How many pages does the book have?"))
+rating = float(input("What is the rating of the book?"))
+
+#print the book details 
+print ("Book Title:", book_title)
+print ("Author:", author)   
+print ("Rating:", rating)
+print ("Pages:", pages)
