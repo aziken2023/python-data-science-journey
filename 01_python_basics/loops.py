a3ebd27e2f0320dd2320493c3d893ee313042671
Joshua Aziken <joshaziken@gmail.com>
2026-07-27 16:08:17 +0100

    created a file for dictionaries

--- 01_python_basics/loops.py
+++ 01_python_basics/loops.py
@@ -1,5 +1,7 @@
 ratings = [4.8, 3.9, 4.5, 2.7, 5.0]
+count = 0
 
+#This loop will check to see the status of each book
 for rating in ratings:
     if rating > 4.0:
         print("Rating:", rating)
@@ -11,12 +13,14 @@ for rating in ratings:
         print("Rating:", rating)
         print("status: poor")
 
+#This loop will check to see if the book is excellent and continue to the next book if it is
 for rating in ratings:
     if rating > 4.0:
         print("This book is excellent")
     continue
 
-count = 0
+#This loop will count the number of excellent books
+
 for rating in ratings:
     if rating > 4.0:
         count += 1
